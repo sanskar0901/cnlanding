f9aeea7132540ac2f79996ca1868a2540b0eb203
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2022-12-16 23:57:27 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
-import React, { Suspense, useEffect } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
+import ClipLoader from "react-spinners/ClipLoader";
 import { OrbitControls, PerspectiveCamera, Stats, Text } from '@react-three/drei'
 import { Environment, Sky } from '@react-three/drei'
 import { getProject } from '@theatre/core'
@@ -17,46 +18,56 @@ import { LoopPingPong } from 'three';
 const demoSheet = getProject('Demo Project', { state: state }).sheet('Demo Sheet')
 function App() {
 
-
+  const [Loading, setLoading] = useState(false)
   useEffect(() => {
+    setLoading(true)
+    setTimeout(() => { { setLoading(false) } }, 5000)
     demoSheet.project.ready.then(() => { demoSheet.sequence.play({ iterationCount: 1, range: [0, 10] }) })
 
   }, [])
   return (
-    <div className="App">
-      <Canvas >
-        <SheetProvider sheet={demoSheet}>
-          {/* <e.fog theatreKey='fog' attach="fog" color="hotpink" near={1} far={10} /> */}
-          {/* <e.PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75} /> */}
-          <ambientLight intensity={2} />
-          <e.pointLight theatreKey="pLight" position={[40, 40, 40]} />
-          <Suspense fallback={null}>
-            <e.group theatreKey="text">
+    <>
 
-              <Text position={[0, 4, -10]} fontSize={6}>
-                Coding Ninjas
-                <meshStandardMaterial color="#fff" toneMapped={false} />
-              </Text>
+      <div className="App">
+        <Canvas >
+          <SheetProvider sheet={demoSheet}>
+            {/* <e.fog theatreKey='fog' attach="fog" color="hotpink" near={1} far={10} /> */}
+            {/* <e.PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75} /> */}
+            <ambientLight intensity={2} />
+            <e.pointLight theatreKey="pLight" position={[40, 40, 40]} />
+            <Suspense fallback={null}>
+              <e.group theatreKey="text">
 
-            </e.group>
-            <e.group theatreKey="text2">
-              <Text position={[0, 4, -30]} fontSize={3}>
-                SRM
-                <meshStandardMaterial color="#fff" toneMapped={false} />
-              </Text>
-            </e.group>
-            <House theatreKey="House" />
+                <Text position={[0, 4, -10]} fontSize={6}>
+                  Coding Ninjas
+                  <meshStandardMaterial color="#fff" toneMapped={false} />
+                </Text>
 
-          </Suspense>
-        </SheetProvider>
-      </Canvas>
-      {/* <motion.h1 className='head'
-        initial={{ scale: 1, x: 0, y: 6000 }}
-        animate={logoControls}
-      >
-        Coding Ninja
-      </motion.h1> */}
-    </div >
+              </e.group>
+              <e.group theatreKey="text2">
+                <Text position={[0, 4, -30]} fontSize={3}>
+                  SRM
+                  <meshStandardMaterial color="#fff" toneMapped={false} />
+                </Text>
+              </e.group>
+              {
+                House ?
+                  <House theatreKey="House" />
+                  : <ClipLoader
+                    color={"#000000"}
+                    loading={Loading}
+                    size={150}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                  />
+              }
+
+            </Suspense>
+          </SheetProvider>
+        </Canvas>
+      </div >
+    </>
+
   );
 }
 
